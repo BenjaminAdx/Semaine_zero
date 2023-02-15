70b382f626973568991dfc954b1f97be79eeccb8
Andrieux Benjamin <andrieux.benjamin1@gmail.com>
2023-02-15 08:09:03 +0100

    exercice pop-up

--- javascript/jour2/app.js
+++ javascript/jour2/app.js
@@ -11,3 +11,21 @@ var onClick = function () {
 
 clicker.addEventListener("click", onClick);
 
+/* var chatEl = document.getElementById("chat");
+var noneEl = document.getElementById("pop-up");
+var popUp = function () {
+    console.log(noneEl.style.display)
+    if (noneEl.style.display != "flex") {
+        noneEl.style.display = "flex";
+    }
+    else { noneEl.style.display = "none" }
+}
+chatEl.addEventListener("click", popUp) */
+
+var chatEl = document.querySelector("#chat");
+var noneEl = document.querySelector(".pop-up");
+function closed() {
+    noneEl.classList.toggle("apparaitre");
+}
+chatEl.addEventListener("click", closed);
+
