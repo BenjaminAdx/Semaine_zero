1d75f7791f5f43bbcf383aacfbcedb83051c86d6
Andrieux Benjamin <andrieux.benjamin1@gmail.com>
2023-02-16 16:53:53 +0100

    jeux guessmynumber

---
+++ javascript/jeux_guessmynumber/app.js
@@ -0,0 +1,63 @@
+let randomnumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomnumber);
+let guesses = document.querySelector('.guesses');
+let lastResult = document.querySelector('.lastResult');
+let lowOrHi = document.querySelector('.lowOrHi');
+
+let guessSubmit = document.querySelector('.guessSubmit');
+let guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+/* fonction principale guessthenumber */
+function submitNumber() {
+    if (guessCount <= 10) {
+        guesses.textContent += guessField.value + " ";
+        if (guessField.value < randomnumber) {
+            lowOrHi.textContent = "Trop petit";
+            lastResult.style.color = "red";
+            lastResult.textContent = "Essaie encore";
+            guessCount++;
+        }
+        else if (guessField.value > randomnumber) {
+            lowOrHi.textContent = "Trop grand";
+            lastResult.style.color = "red";
+            lastResult.textContent = "Essaie encore";
+            guessCount++;
+        }
+        else {
+            lastResult.style.color = "green";
+            lastResult.textContent = "Bravo";
+            guessCount++;
+            guessSubmit.disabled = true;
+            lowOrHi.innerHTML = "<button class='reset'>Reset</button>";
+            resetall();
+        }
+    }
+    else {
+        lastResult.textContent = "Game Over";
+        guessSubmit.disabled = true;
+        lowOrHi.innerHTML = "<button class='reset'>Reset</button>";
+        resetall();
+    }
+    guessField.value = "";
+};
+/* fonction reset à appeler */
+function resetall() {
+    let resetButton = document.querySelector(".reset");
+    function resetgame() {
+        guesses.textContent = "";
+        lastResult.textContent = "";
+        lowOrHi.innerHTML = "";
+        guessSubmit.disabled = false;
+        guessCount = 1;
+        randomnumber = Math.floor(Math.random() * 100) + 1;
+        console.log(randomnumber);
+    };
+    resetButton.addEventListener("click", resetgame);
+}
+/* écoute click submit */
+guessSubmit.addEventListener("click", submitNumber);
+
+
+
+
