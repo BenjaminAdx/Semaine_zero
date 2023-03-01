f45d9e2f2d176c7b53728d9fe7e17cf413f3817b
benjaminadx <andrieux.benjamin1@gmail.com>
2023-03-01 14:45:14 +0100

    menu burger v2

--- javascript/jour3/app.js
+++ javascript/jour3/app.js
@@ -2,7 +2,6 @@ let divSpan = document.querySelector(".menu")
 let topEl = document.querySelector(".top")
 let middle = document.querySelector(".middle")
 let bottom = document.querySelector(".bottom")
-let linksaEl = document.querySelectorAll("a")
 let linksEl = document.querySelector(".links")
 
 
@@ -10,9 +9,6 @@ function menuburger() {
     topEl.classList.toggle("span1on");
     middle.classList.toggle("span2on");
     bottom.classList.toggle("span3on");
-    for (let i = 0; i < linksaEl.length; i++) {
-        linksaEl[i].classList.toggle("linkson");
-    };
     linksEl.classList.toggle("linksposition");
 }
 divSpan.addEventListener("click", menuburger)
